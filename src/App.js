ccbe41a36d889003e6e5b741b78e015b10a8479e
Mihika.Sharma <mihika.sharma@ril.com>
2018-03-21 12:46:41 +0530

    Dynamically changing the styles of class, using radium  Changes to be committed: 	modified:   package-lock.json 	modified:   package.json 	modified:   src/App.css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import Radium,{StyleRoot} from 'radium';
 import Person from './Person/Person'
 
 class App extends Component {
   state = {
     persons:[
       {id:'asd',name:'Mihika',age:23},
-      {id:'dsf',name:'Neha',age:20}
+      {id:'dsf',name:'Neha',age:20},
+      {id:'fgh',name:'Nisha',age:48}
     ],
     showPersons : false
   }
@@ -38,11 +40,16 @@ class App extends Component {
   }
   render() {
     const style = {
-      backgroundColor  : 'white',
+      backgroundColor  : 'green',
+      color:'white',
       font : 'inherit',
       border : '1x solid blue',
       padding : '8px',
-      cursor : 'pointer'
+      cursor : 'pointer',
+      ':hover' : {
+        backgroundColor: 'lightGreen',
+        color : 'black'
+      }
     }
     let persons = null;
     if(this.state.showPersons){
@@ -59,18 +66,33 @@ class App extends Component {
           })}
        </div> 
       )
+      style.backgroundColor='red';
+      style[':hover']={
+        backgroundColor : 'salmon',
+        color : 'black'
+      }
     }
+    const classes = [];
+    if(this.state.persons.length<=2){
+      classes.push('red')
+    }
+    if(this.state.persons.length<=1){
+      classes.push('bold');
+    }
+    
     return (
+      <StyleRoot>
       <div className="App">
        <h1>Hi I am a a react app</h1>
-       <p>This is really working</p>
+       <p className={classes.join(' ')}>This is really working</p>
        <button onClick={()=>this.togglePersonsHandler()}
         style={style}
        >Toggle Persons</button>
        {persons}
       </div>
+      </StyleRoot>
     );
   }
 }
 
-export default App;
+export default Radium(App);
